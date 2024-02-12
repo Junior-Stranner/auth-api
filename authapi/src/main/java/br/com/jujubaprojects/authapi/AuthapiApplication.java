c0bd600fa5603c4d8abbcfc52b635825cc8b57ff
Junior-Stranner <Ju-stranner@outlook.com>
2024-02-12 15:53:42 -0300

    start the coding now

---
+++ authapi/src/main/java/br/com/jujubaprojects/authapi/AuthapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jujubaprojects.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthapiApplication.class, args);
+	}
+
+}
---
+++ authapi/src/test/java/br/com/jujubaprojects/authapi/AuthapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jujubaprojects.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
